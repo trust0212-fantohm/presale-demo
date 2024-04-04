e53a04f2e4dac94685622b661f9a2dda3f60bfa8
icetrust0212 <zaza.liveedu@gmail.com>
2024-04-04 17:05:09 -0400

    updated font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,30 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    screens: {
-      xl: { max: "1535px" },
-      lg: { max: "1279px" },
-      md: { max: "991px" },
-      sm: { max: "767px" },
-      xs: { max: "539px" },
-    },
-    extend: {
-      backgroundImage: {},
-      fontFamily: {
-        impact: "Impact",
-        rubik: "Rubik, sans-serif",
-        numans: 'Numans, sans-serif',
-        roboto: 'Roboto, sans-serif'
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		screens: {
+			xl: { max: "1535px" },
+			lg: { max: "1279px" },
+			md: { max: "991px" },
+			sm: { max: "767px" },
+			xs: { max: "539px" },
+		},
+		extend: {
+			backgroundImage: {},
+			fontFamily: {
+				impact: "Impact",
+				rubik: "Rubik, sans-serif",
+				numans: "Numans, sans-serif",
+				norwester: "Norwester",
+				roboto: "Roboto, sans-serif",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
