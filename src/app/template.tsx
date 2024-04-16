5234e47ec6226ea532c3e396609b49bafa16509d
trust0212-fantohm <zaza.liveedu@gmail.com>
2024-04-16 08:14:03 -0700

    added animation effect using framer-motion

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,13 +1,22 @@
 'use client';
 
 import { PropsWithChildren } from 'react';
+import { motion } from 'framer-motion';
 import PlayButton from '@/components/PlayButton';
 
 export default function RootTemplate({ children }: PropsWithChildren) {
   return (
     <div className='relative'>
       <PlayButton className='absolute right-8 top-5 z-20' autoPlay={true} />
-      {children}
+      <motion.div
+        className='relative'
+        initial={{ opacity: 1, x: -0, y: 0, filter: 'blur(10px)' }}
+        animate={{ opacity: 1, x: -0, y: 0, filter: 'blur(0px)' }}
+        exit={{ opacity: 1, x: -0, y: 0, filter: 'blur(10px)' }}
+        transition={{ type: 'spring' }}
+      >
+        {children}
+      </motion.div>
     </div>
   );
 }
